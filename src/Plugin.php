cdfbce8936b7eca81715d21ed5892b1ccfc1aaab
SilverFire - Dmytro Naumenko <d.naumenko.a@gmail.com>
2025-01-28 16:54:43 +0200

    Do not disable default resolvers and downloaders

--- src/Plugin.php
+++ src/Plugin.php
@@ -10,10 +10,6 @@ use Composer\Plugin\Capable;
 use Composer\Plugin\PluginInterface;
 use cweagans\Composer\Capability\Downloader\DownloaderProvider;
 use cweagans\Composer\Capability\Resolver\ResolverProvider;
-use cweagans\Composer\Downloader\ComposerDownloader;
-use cweagans\Composer\Resolver\Dependencies;
-use cweagans\Composer\Resolver\PatchesFile;
-use cweagans\Composer\Resolver\RootComposer;
 use hiqdev\ComposerCiDeps\Provider\PullRequestsDownloaderProvider;
 use hiqdev\ComposerCiDeps\Provider\PullRequestsResolverProvider;
 
@@ -29,29 +25,6 @@ class Plugin implements PluginInterface, EventSubscriberInterface, Capable
 
     public function activate(Composer $composer, IOInterface $io)
     {
-        $extra = $composer->getPackage()->getExtra();
-        if (isset($extra['composer-patches'])) {
-            return;
-        }
-
-        $extra['composer-patches'] = [
-            'disable-resolvers' => [
-                '\\' . RootComposer::class,
-                '\\' . PatchesFile::class,
-                '\\' . Dependencies::class,
-            ],
-            'disable-downloaders' => [
-                '\\' . ComposerDownloader::class,
-            ],
-        ];
-
-        $plugins = $composer->getPluginManager()->getPlugins();
-        foreach ($plugins as $plugin) {
-            if ($plugin instanceof \cweagans\Composer\Plugin\Patches) {
-                $plugin->configure($extra, 'composer-patches');
-                break;
-            }
-        }
     }
 
     public function deactivate(Composer $composer, IOInterface $io)
